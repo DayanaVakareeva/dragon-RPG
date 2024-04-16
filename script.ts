2e6d192ed13948cb3de442afeadb21ddfdbc12cc
DayanaVakareeva <136321295+DayanaVakareeva@users.noreply.github.com>
2024-04-16 20:48:04 +0300

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,10 @@
+let xp: number = 0;
+let health: number = 100;
+let gold: number = 50;
+let currentWeapon: number = 0;
+let fighting:any;
+let monsterHealth: any;
+let inventory: string[] = ['stick']
+
+let button1: HTMLButtonElement | null =  document.querySelector('#button1')
+
